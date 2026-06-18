7a77dd9137c3d30098066738d5037f607f65e1df
Greendomo <35412174+Greendomo@users.noreply.github.com>
2026-06-18 20:15:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts("./quickdot-files.js");
 
-const CACHE_NAME = "quickdot-cache-v126";
+const CACHE_NAME = "quickdot-cache-v127";
 const APP_SHELL = self.QUICKDOT_APP_SHELL;
 
 self.addEventListener("install", (event) => {
